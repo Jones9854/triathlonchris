953cf9a80d6cc2eb9e7f5edbe2da853788495142
Christian Jones <jones9854@gmail.com>
2018-07-23 19:59:52 +0100

    added IM wales, favicon, and removed prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     description: 'Triathlon and software blog.',
     siteUrl: 'http://www.triathlonchris.com/',
   },
-  pathPrefix: '/triathlonchris',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
