b6bf818e6c2f917ca8a2d0548c3b0678aded4144
Christian Jones <jones9854@hotmail.com>
2019-10-03 21:30:55 +0100

    update

--- src/app/models/result-model.ts
+++ src/app/models/result-model.ts
@@ -1,7 +1,11 @@
 export class ResultModel {
     RaceName: string;
     Swim: number;
+    SwimTime?: Date;
     Bike: number;
+    BikeTime?: Date;
     Run: number;
+    RunTime?: Date;
     Overall: number;
+    OverallTime?: Date;
 }
