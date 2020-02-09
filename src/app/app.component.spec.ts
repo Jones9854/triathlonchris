7c7beb5d6a29b4900d3f84a5b19cc7f6b9b7faf3
Christian Jones <jones9854@hotmail.com>
2020-02-09 12:27:43 +0000

    rename to triathlonchris from tri-chris

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Tri-Chris'`, () => {
+  it(`should have as title 'triathlonchris'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Tri-Chris');
+    expect(app.title).toEqual('triathlonchris');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Tri-Chris!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to triathlonchris!');
   });
 });
